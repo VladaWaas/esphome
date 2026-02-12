61ee38e74411e3d93212bee2c0a7fea2f8a8291b
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-12 16:39:50 +0100

    Update __init__.py

--- components/ls027b7dh01/__init__.py
+++ components/ls027b7dh01/__init__.py
@@ -1,1 +1,35 @@
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome import pins
+from esphome.components import display, spi
+from esphome.const import (
+    CONF_ID,
+    CONF_LAMBDA,
+    CONF_PAGES,
+)
 
+CODEOWNERS = ["@esphome/core"]
+AUTO_LOAD = ["display"]
+DEPENDENCIES = ["spi"]
+
+ls027b7dh01_ns = cg.esphome_ns.namespace("ls027b7dh01")
+LS027B7DH01 = ls027b7dh01_ns.class_(
+    "LS027B7DH01", cg.PollingComponent, spi.SPIDevice, display.DisplayBuffer
+)
+
+CONFIG_SCHEMA = (
+    display.FULL_DISPLAY_SCHEMA.extend(
+        {
+            cv.GenerateID(): cv.declare_id(LS027B7DH01),
+        }
+    )
+    .extend(cv.polling_component_schema("1s"))
+    .extend(spi.spi_device_schema(cs_pin_required=True))
+)
+
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await spi.register_spi_device(var, config)
+    await display.register_display(var, config)
