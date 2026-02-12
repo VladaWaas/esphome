83350ff9bf6f1e6a9bc0980e8ccfdda101bb2ef6
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-12 16:58:03 +0100

    Refactor display.py to re-export from __init__.py

--- components/ls027b7dh01/display.py
+++ components/ls027b7dh01/display.py
@@ -1,4 +1,2 @@
-# This file makes ls027b7dh01 available as a display platform
-from . import ls027b7dh01_ns, LS027B7DH01, CONFIG_SCHEMA, to_code
-
-CODEOWNERS = ["@esphome/core"]
+# Re-export everything from __init__.py
+from . import *
