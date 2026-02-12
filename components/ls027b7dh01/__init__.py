65838eb2c293ec87595c4fd5aa09cb6ea3c09eaf
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-12 17:11:18 +0100

    Remove component registration in to_code function
    Removed registration of the component from the to_code function.

--- components/ls027b7dh01/__init__.py
+++ components/ls027b7dh01/__init__.py
@@ -31,6 +31,5 @@ CONFIG_SCHEMA = (
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
     await esphome_display.register_display(var, config)
