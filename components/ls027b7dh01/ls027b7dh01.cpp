f49e7d6d4a2a485569b80d3190d2db36a805fbf8
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-13 14:26:33 +0100

    Update ls027b7dh01.cpp

--- components/ls027b7dh01/ls027b7dh01.cpp
+++ components/ls027b7dh01/ls027b7dh01.cpp
@@ -100,7 +100,7 @@ void LS027B7DH01::write_display_data_() {
       uint8_t data = this->buffer_[offset + i];
       
       // LSB first - reverse bits
-      data = this->reverse_bits_(data);
+      // data = this->reverse_bits_(data);
       this->write_byte(data);
     }
     
@@ -147,4 +147,5 @@ void LS027B7DH01::toggle_vcom_() {
 
 }  // namespace ls027b7dh01
 }  // namespace esphome
+
 
