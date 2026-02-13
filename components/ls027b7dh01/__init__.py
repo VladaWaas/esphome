f0ed6a0b49155578ede8decf8b1cac17b2e04428
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-13 10:11:24 +0100

    Update LS027B7DH01 component configuration
    Refactor CONFIG_SCHEMA to include optional lambda and update interval.

--- components/ls027b7dh01/__init__.py
+++ components/ls027b7dh01/__init__.py
@@ -1,31 +1,31 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import display, spi
-from esphome.const import CONF_ID, CONF_LAMBDA
+from esphome.components import spi
+from esphome import pins
+from esphome.const import CONF_ID, CONF_LAMBDA, CONF_UPDATE_INTERVAL
 
 DEPENDENCIES = ["spi"]
 
-
 ls027b7dh01_ns = cg.esphome_ns.namespace("ls027b7dh01")
 LS027B7DH01 = ls027b7dh01_ns.class_(
-    "LS027B7DH01", 
-    cg.PollingComponent, 
-    spi.SPIDevice,
-    display.DisplayBuffer
+    "LS027B7DH01",
+    cg.PollingComponent,
+    spi.SPIDevice
 )
 
-CONFIG_SCHEMA = (
-    display.FULL_DISPLAY_SCHEMA.extend(
-        {
-            cv.GenerateID(): cv.declare_id(LS027B7DH01),
-        }
-    )
-    .extend(cv.polling_component_schema("1s"))
-    .extend(spi.spi_device_schema(cs_pin_required=True))
-)
+CONFIG_SCHEMA = cv.Schema({
+    cv.GenerateID(): cv.declare_id(LS027B7DH01),
+    cv.Optional(CONF_LAMBDA): cv.lambda_,
+    cv.Optional(CONF_UPDATE_INTERVAL, default="1s"): cv.update_interval,
+}).extend(cv.polling_component_schema("1s")).extend(spi.spi_device_schema(cs_pin_required=True))
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
-    await display.register_display(var, config)
+    
+    if CONF_LAMBDA in config:
+        lambda_ = await cg.process_lambda(
+            config[CONF_LAMBDA], [], return_type=cg.void
+        )
+        cg.add(var.set_writer(lambda_))
