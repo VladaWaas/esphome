d757096bb036d0a2bca8db257f443765c62c09ee
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-13 09:56:54 +0100

    Update ls027b7dh01.h

--- components/ls027b7dh01/ls027b7dh01.h
+++ components/ls027b7dh01/ls027b7dh01.h
@@ -7,36 +7,40 @@
 namespace esphome {
 namespace ls027b7dh01 {
 
+// Display rozměry
 static const uint16_t LS027B7DH01_WIDTH = 400;
 static const uint16_t LS027B7DH01_HEIGHT = 240;
 
+// Sharp LCD příkazy
 static const uint8_t SHARP_LCD_BIT_WRITECMD = 0x01;
 static const uint8_t SHARP_LCD_BIT_VCOM = 0x02;
 static const uint8_t SHARP_LCD_BIT_CLEAR = 0x04;
 
 class LS027B7DH01 : public display::DisplayBuffer,
-                    public spi::SPIDevice<spi::BIT_ORDER_LSB_FIRST, spi::CLOCK_POLARITY_LOW,
-                                          spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_2MHZ> {
+                    public spi::SPIDevice<spi::BIT_ORDER_LSB_FIRST, 
+                                          spi::CLOCK_POLARITY_LOW,
+                                          spi::CLOCK_PHASE_LEADING, 
+                                          spi::DATA_RATE_2MHZ> {
  public:
   void setup() override;
   void dump_config() override;
   void update() override;
-  void fill(Color color) override;
   
-  display::DisplayType get_display_type() override { return display::DisplayType::DISPLAY_TYPE_BINARY; }
+  display::DisplayType get_display_type() override { 
+    return display::DisplayType::DISPLAY_TYPE_BINARY; 
+  }
 
  protected:
   void draw_absolute_pixel_internal(int x, int y, Color color) override;
   int get_height_internal() override { return LS027B7DH01_HEIGHT; }
   int get_width_internal() override { return LS027B7DH01_WIDTH; }
-  void init_internal_(uint32_t buffer_length) override {
-    this->init_buffer_(this->buffer_);
-  }
+  
   void write_display_data_();
   void clear_display_();
   void toggle_vcom_();
+  uint8_t reverse_bits_(uint8_t b);
   
- 
+  uint8_t *buffer_{nullptr};
   bool vcom_state_{false};
   
   static const size_t BUFFER_SIZE = (LS027B7DH01_WIDTH / 8) * LS027B7DH01_HEIGHT;
@@ -44,8 +48,3 @@ class LS027B7DH01 : public display::DisplayBuffer,
 
 }  // namespace ls027b7dh01
 }  // namespace esphome
-
-
-
-
-
