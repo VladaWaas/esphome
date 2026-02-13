8a9ca9889f15f6121fec7d5176ce2b6a6047eb6f
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-13 10:28:18 +0100

    Register display component instead of generic component

--- components/ls027b7dh01/display.py
+++ components/ls027b7dh01/display.py
@@ -14,6 +14,6 @@ CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
-    await cg.register_component(var, config)
+   
     await display.register_display(var, config)
     await spi.register_spi_device(var, config)
