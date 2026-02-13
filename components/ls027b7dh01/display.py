f0e2ff40048b72da7e32c937d436ee9fa2eeea1e
VladaWaas <97882474+VladaWaas@users.noreply.github.com>
2026-02-13 10:23:44 +0100

    Refactor display.py to include SPI dependencies

--- components/ls027b7dh01/display.py
+++ components/ls027b7dh01/display.py
@@ -1,8 +1,19 @@
-# Tento soubor je kvůli ESPHome display platformě
-# Re-exportuje komponentu jako display platform
+import esphome.codegen as cg
+import esphome.config_validation as cv
+from esphome.components import display, spi
+from esphome.const import CONF_ID, CONF_LAMBDA
 
-from esphome.components import display
-from . import ls027b7dh01_ns, LS027B7DH01
+from . import ls027b7dh01_ns, LS027B7DH01, CONF_LS027B7DH01_ID
 
-# Registrace jako display platforma
-PLATFORM_SCHEMA = None  # Používáme schema z __init__.py
+DEPENDENCIES = ["spi"]
+
+# Platform schema pro display
+CONFIG_SCHEMA = display.BASIC_DISPLAY_SCHEMA.extend({
+    cv.GenerateID(): cv.declare_id(LS027B7DH01),
+}).extend(cv.polling_component_schema("1s")).extend(spi.spi_device_schema(cs_pin_required=True))
+
+async def to_code(config):
+    var = cg.new_Pvariable(config[CONF_ID])
+    await cg.register_component(var, config)
+    await display.register_display(var, config)
+    await spi.register_spi_device(var, config)
